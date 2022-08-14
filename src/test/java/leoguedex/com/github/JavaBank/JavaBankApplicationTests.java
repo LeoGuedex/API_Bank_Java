63a6325950e32aafea91ef4fee5e79ae4fe78086
LeoGuedex <leocguedex@gmail.com>
2022-08-14 00:38:16 -0300

    first commit

---
+++ src/test/java/leoguedex/com/github/JavaBank/JavaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package leoguedex.com.github.JavaBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
